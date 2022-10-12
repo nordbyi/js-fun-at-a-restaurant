a6c271ba7b09e88f4b93ae25741dbfada3e17c8a
Ian Nordby <ian.c.nordby@gmail.com>
2022-10-12 15:31:48 -0600

    Add answers

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,30 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  return {name, price, type}
+}
+
+const addIngredients = function(ingredient, arr) {
+  return arr.includes(ingredient) ?  arr :  arr.push(ingredient)
+}
+
+const formatPrice = function(price) {
+  return `$${price}`
+}
+
+const decreasePrice = (price) => price * .9
+
+const createRecipe = (title, ingredients, type) => {
+  return {title, ingredients, type}
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
