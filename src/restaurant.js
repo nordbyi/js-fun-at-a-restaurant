dd5089223d9b47780fb1c9a0dc943ad2ec014bf4
Ian Nordby <ian.c.nordby@gmail.com>
2022-10-12 18:04:04 -0600

    Add functions to pass tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,28 @@
+function createRestaurant(name, menuObj = {breakfast: [], lunch : [], dinner : []}) {
+  return {
+    name,
+    menus: menuObj
+  }
+}
 
+const addMenuItem = function(restaurant, item) {
+  return restaurant.menus[item.type].includes(item)
+  ? restaurant
+  : restaurant.menus[item.type].push(item)
+}
+
+const removeMenuItem = (restaurantObj, itemName, itemType) => {
+  var mealArr = restaurantObj.menus[itemType]
+  if(!mealArr.some(el => el.name === itemName)) {
+    return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
+  } else {
+    mealArr.splice(mealArr.findIndex(el => el.name === itemName) ,1)
+    return `No one is eating our ${itemName} - it has been removed from the ${itemType} menu!`
+  }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem,
 }
