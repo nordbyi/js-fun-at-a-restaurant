e9885b8db7b24b9ea996f31a8305ef2755206cde
Ian Nordby <ian.c.nordby@gmail.com>
2022-10-12 18:30:14 -0600

    Add constructor and methods to pass tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,18 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name
+    this.restaurant = restaurant
+  }
+  greetCustomer(customerName, bool) {
+    return bool
+    ? `Good morning, ${customerName}!`
+    : `Hello, ${customerName}!`
+  }
+
+  checkForFood(foodItem) {
+    return this.restaurant.menus[foodItem.type].some(el => el.name === foodItem.name)
+    ? `Yes, we're serving ${foodItem.name} today!`
+    : `Sorry, we aren't serving ${foodItem.name} today.`
   }
 }
 
